d36e584ea689eeace6ed719d8b4a48518bbaee26
Humberto07livramento <148144477+Humberto07livramento@users.noreply.github.com>
2024-07-31 08:50:34 -0300

    Update tratando duplicidades_datas_sumarizando.R

--- tratando duplicidades_datas_sumarizando.R
+++ tratando duplicidades_datas_sumarizando.R
@@ -1,9 +1,9 @@
 
 # Carregando os pacotes ---------------------------------------------------
 
-library(rio)        # pacote para importação de dados
-library(tidyverse)  # pacote para manipulação de dados
-library(lubridate)  # pacote para operação com datas
+library(rio)        # pacote para importaÃ§Ã£o de dados
+library(tidyverse)  # pacote para manipulaÃ§Ã£o de dados
+library(lubridate)  # pacote para operaÃ§Ã£o com datas
 library(janitor)    # pacote para limpeza de dados
 
 # Definindo a pasta de trabalho -------------------------------------------
@@ -16,7 +16,7 @@ getwd()
 
 nindinet <- import("C:\\opasR\\DADOS\\NINDINET_MODULO4.xlsx")
 
-# Inspecionando os tipos das variï¿½veis ------------------------------------
+# Inspecionando os tipos das variÃ¯Â¿Â½veis ------------------------------------
 
 glimpse(nindinet)
 
@@ -30,23 +30,23 @@ lubridate::today() + hours(72)
 
 hoje <- "03/04/2024" # Criando um objeto 
 hoje                 # Visualizando o objeto
-class(hoje)          # FunçãO para ver o tipo do objeto
+class(hoje)          # FunÃ§Ã£O para ver o tipo do objeto
 
 hoje <- dmy("03/04/2024")
 class(hoje)
 hoje
 
-# Um pouco sobre períodos, durações e diferença
+# Um pouco sobre perÃ­odos, duraÃ§Ãµes e diferenÃ§a
 
-# Função tail() para ver as últimas observações do dataset
+# FunÃ§Ã£o tail() para ver as Ãºltimas observaÃ§Ãµes do dataset
 
-# Função as.period() representam unidades humanas como dias e semanas
+# FunÃ§Ã£o as.period() representam unidades humanas como dias e semanas
 tail(lubridate::as.period(nindinet$DT_NOTIFIC - nindinet$DT_SIN_PRI))
 
-# Função as.duration() representa um número exato de segundos.
+# FunÃ§Ã£o as.duration() representa um nÃºmero exato de segundos.
 tail(lubridate::as.duration(nindinet$DT_NOTIFIC - nindinet$DT_SIN_PRI))
 
-# Função as.difftime() representa a diferença em segundos
+# FunÃ§Ã£o as.difftime() representa a diferenÃ§a em segundos
 tail(lubridate::as.difftime(nindinet$DT_NOTIFIC - nindinet$DT_SIN_PRI))
 
 # Ajustando os formatos de datas, com {lubridate} -------------------------
@@ -56,41 +56,41 @@ nindinet <- nindinet |>                          # dados e operador pipe
          DT_SIN_PRI = ymd(DT_SIN_PRI),
          DT_INVEST  = ymd(DT_INVEST),
          DT_ENCERRA = ymd(DT_ENCERRA),
-         DT_DIGITA  = dmy(DT_DIGITA)   # Atenção ao formato dmy()
+         DT_DIGITA  = dmy(DT_DIGITA)   # AtenÃ§Ã£o ao formato dmy()
   ) 
 
 nindinet |> select(starts_with("DT_")) |>  glimpse()
 
-# Criando as variáveis dos cálculo de oportunidade -----------------------
+# Criando as variÃ¡veis dos cÃ¡lculo de oportunidade -----------------------
 
-# Definições:
-# Oportunidade de notificação: Intervalo entre DT_SIN_PRI e DT_NOTIFIC
-# Oportunidade de digitação: Intervalo entre DT_NOTIFIC e DT_DIGITA
+# DefiniÃ§Ãµes:
+# Oportunidade de notificaÃ§Ã£o: Intervalo entre DT_SIN_PRI e DT_NOTIFIC
+# Oportunidade de digitaÃ§Ã£o: Intervalo entre DT_NOTIFIC e DT_DIGITA
 # Oportunidade de encerramento: Intervalo entre DT_NOTIFIC e DT_ENCERRA
 
-# Problema com a operação simples de subtração entre datas <drtn>
+# Problema com a operaÃ§Ã£o simples de subtraÃ§Ã£o entre datas <drtn>
 nindinet <- nindinet |>
   mutate(oportunidade_notificacao = DT_NOTIFIC - DT_SIN_PRI)
 nindinet 
 
 glimpse(nindinet$oportunidade_notificacao)
 
-## Criando variáveis para as oportunidades, com a função mutate()
+## Criando variÃ¡veis para as oportunidades, com a funÃ§Ã£o mutate()
 
 nindinet <- nindinet |>
   mutate(oportunidade_notificacao  = as.numeric(DT_NOTIFIC - DT_SIN_PRI),
          oportunidade_digitacao    = as.numeric(DT_DIGITA - DT_NOTIFIC),
          oportunidade_encerramento = as.numeric(DT_ENCERRA - DT_NOTIFIC))
 
-## Inspecionando as variáveis criadas
+## Inspecionando as variÃ¡veis criadas
 
 nindinet |> select(starts_with("oportunidade")) |> glimpse()
 
 
-# Avaliações das oportunidades -----------------------------------------
+# AvaliaÃ§Ãµes das oportunidades -----------------------------------------
 
-mean(nindinet$oportunidade_notificacao)   # Cálculo da média
-median(nindinet$oportunidade_notificacao) # Cálculo da mediana
+mean(nindinet$oportunidade_notificacao)   # CÃ¡lculo da mÃ©dia
+median(nindinet$oportunidade_notificacao) # CÃ¡lculo da mediana
 
 # Criando um resumo por ID_AGRAVO
 
@@ -101,7 +101,7 @@ resumo_oportunidade_digitacao <- nindinet |>
 
 resumo_oportunidade_digitacao
 
-# Criando uma variável avaliando o encerramento das notificação
+# Criando uma variÃ¡vel avaliando o encerramento das notificaÃ§Ã£o
 
 nindinet <- nindinet |>
   mutate(avaliacao_encerramento = if_else(oportunidade_encerramento <= 60,
@@ -116,120 +116,120 @@ resumo_encerramento <- nindinet |>
 print(resumo_encerramento)
 print(resumo_oportunidade_digitacao)
 
-# Análise de duplicidades -------------------------------------------------
+# AnÃ¡lise de duplicidades -------------------------------------------------
 
-# Definições:
+# DefiniÃ§Ãµes:
 # A duplicidade ocorre quando, no universo de todos os registros,
-# um mesmo evento (com o mesmo indivíduo) foi notificado mais de uma vez.
+# um mesmo evento (com o mesmo indivÃ­duo) foi notificado mais de uma vez.
 
 
-# Buscando duplicidades com a função janitor::get_dupes()
+# Buscando duplicidades com a funÃ§Ã£o janitor::get_dupes()
 
-# 5 (Cinco chaves de identificação)
+# 5 (Cinco chaves de identificaÃ§Ã£o)
 
 duplicidades5 <- nindinet |> get_dupes(NM_PACIENT, DT_NASC,
                                        NM_MAE_PAC, ID_AGRAVO, DT_SIN_PRI)
 
-# 4 (Quatro chaves de identificação)
+# 4 (Quatro chaves de identificaÃ§Ã£o)
 
 duplicidades4 <- nindinet |> get_dupes(NM_PACIENT, DT_NASC,
                                        NM_MAE_PAC, ID_AGRAVO)
 
-# 3 (Trinta chaves de identificação)
+# 3 (Trinta chaves de identificaÃ§Ã£o)
 
 duplicidades3 <- nindinet |> get_dupes(NM_PACIENT, DT_NASC, NM_MAE_PAC)
 
 duplicidades_agravo <- nindinet |> get_dupes(ID_AGRAVO, DT_NOTIFIC, DT_NASC)
 
-# 2 (Duas chaves de identificação)
+# 2 (Duas chaves de identificaÃ§Ã£o)
 
 duplicidades2 <- nindinet |> get_dupes(NM_PACIENT, DT_NASC)
 
 
 
-# Criar uma base de dados sem duplicidades, com a funï¿½ï¿½o distinct() ------
+# Criar uma base de dados sem duplicidades, com a funÃ¯Â¿Â½Ã¯Â¿Â½o distinct() ------
 
 
-# Nova base somente com as variáveis que diferenciam os registros
+# Nova base somente com as variÃ¡veis que diferenciam os registros
 
 sem_duplicidades <- nindinet |>
   distinct(NM_PACIENT, DT_NASC)
 
 view(sem_duplicidades)
 
-# Com todas as variáveis da base de dados
+# Com todas as variÃ¡veis da base de dados
 
 sem_duplicidades2 <- nindinet |>
   distinct(NM_PACIENT, DT_NASC, .keep_all = TRUE)
 view(sem_duplicidades2)
 
-# Análise de inconsistências ----------------------------------------------
+# AnÃ¡lise de inconsistÃªncias ----------------------------------------------
 
-# Definições
-# Percentual de variáveis relacionadas que apresentam valores coerentes,
-# não contraditidórios entre si
+# DefiniÃ§Ãµes
+# Percentual de variÃ¡veis relacionadas que apresentam valores coerentes,
+# nÃ£o contraditidÃ³rios entre si
 
-## Busca de inconsistências nas variáveis  CS_ESCOL_N  e NU_IDADE_N
+## Busca de inconsistÃªncias nas variÃ¡veis  CS_ESCOL_N  e NU_IDADE_N
 
-## Dicionário de dados SINAN CS_ESCOL_N
-## Preenchido automaticamente com a Categoria "10 - não se aplica"
+## DicionÃ¡rio de dados SINAN CS_ESCOL_N
+## Preenchido automaticamente com a Categoria "10 - nÃ£o se aplica"
 ## quando idade menor a 7 anos de idade.
-## Quando caso notificado > 7 anos, campo não pode ser
-## preenchido com categoria "10 - não se aplica".
+## Quando caso notificado > 7 anos, campo nÃ£o pode ser
+## preenchido com categoria "10 - nÃ£o se aplica".
 # 
 
 # Busca de casos menores de 10 anos com escolaridade superior a 5 anos
 
-# Definindo o total de notificações
+# Definindo o total de notificaÃ§Ãµes
 
 nrow(nindinet)
 
 
-# Buscando a inconsistência e contabilizando
+# Buscando a inconsistÃªncia e contabilizando
 
 nindinet |>
   filter(NU_IDADE_N >= 4007, NU_IDADE_N <= 4009) |> 
   group_by(CS_ESCOL_N) |>
   summarise(casos = n())
 
-# Filtrando os registros com inconsistência
+# Filtrando os registros com inconsistÃªncia
 
 nindinet |>
   filter(NU_IDADE_N >= 4007, NU_IDADE_N <= 4009) |> 
   filter(CS_ESCOL_N == "05" | CS_ESCOL_N == "06" | CS_ESCOL_N == "07") |>
   select(NU_NOTIFIC, NM_PACIENT, DT_NASC, NU_IDADE_N, CS_ESCOL_N)
 
-# Criando uma variável para a inconsistência
+# Criando uma variÃ¡vel para a inconsistÃªncia
 
 nindinet <- nindinet |>
   mutate(inc_escolaridade = case_when(NU_IDADE_N >= 4007
                                       & NU_IDADE_N <= 4009 &
-                                        CS_ESCOL_N == "05" ~ "Inconsistência",
+                                        CS_ESCOL_N == "05" ~ "InconsistÃªncia",
                                       NU_IDADE_N >= 4007
                                       & NU_IDADE_N <= 4009 &
-                                        CS_ESCOL_N == "06" ~ "Inconsistência",
+                                        CS_ESCOL_N == "06" ~ "InconsistÃªncia",
                                       NU_IDADE_N >= 4007
                                       & NU_IDADE_N <= 4009 &
-                                        CS_ESCOL_N == "07" ~ "Inconsistência",
+                                        CS_ESCOL_N == "07" ~ "InconsistÃªncia",
                                       NU_IDADE_N >= 4007
                                       & NU_IDADE_N <= 4009 &
-                                        CS_ESCOL_N == "08" ~ "Inconsistência",
+                                        CS_ESCOL_N == "08" ~ "InconsistÃªncia",
                                       CS_ESCOL_N == "09" ~ "Ignorado",
                                       is.na(CS_ESCOL_N) ~ "Ignorado",
                                       TRUE ~ "Consistente"))
 
 
 
-# Análise de completitude -------------------------------------------------
+# AnÃ¡lise de completitude -------------------------------------------------
 
-## Avaliando a variável CS_RACA
+## Avaliando a variÃ¡vel CS_RACA
 
 ## Avaliando apenas os registros vazios
 
 nindinet |>                                 # base de dados
-  summarise(                                # função de resumo
+  summarise(                                # funÃ§Ã£o de resumo
     completo = sum(!is.na(CS_RACA)),        # soma os diferentes(!) de vazio (NA)
-    total_notificacoes = n(),               # conta todas as notificão
+    total_notificacoes = n(),               # conta todas as notificÃ£o
     missing = sum(is.na(CS_RACA)),          # soma os iguais a vazio (NA)
     taxa_completitude = (completo / total_notificacoes) * 100)
 
@@ -240,7 +240,7 @@ nindinet |>
   summarise(notificacoes = n())
 
 
-## Incorporando na avaliação os valores preenchidos como "9 - Ignorado"
+## Incorporando na avaliaÃ§Ã£o os valores preenchidos como "9 - Ignorado"
 
 nindinet |>
   summarise(
@@ -249,7 +249,7 @@ nindinet |>
     missing = sum(is.na(CS_RACA) | CS_RACA == "9"),
     taxa_completitude = (completo / total_notificacoes) * 100)
 
-# Buscando valores vazios ou ignorados na variável ID_OCUPA_N
+# Buscando valores vazios ou ignorados na variÃ¡vel ID_OCUPA_N
 
 nindinet |>
   summarise(
